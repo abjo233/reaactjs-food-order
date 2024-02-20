cde81721acaf1130bd7bf2f2ef5b295d3032c55c
Abhijeet Joshi <abjoshi@redhat.com>
2024-02-20 16:34:25 +0530

    added cart modal

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
+import Cart from './components/Cart/Cart';
+
 
 function App() {
-  return (
+	return (
     <CartProvider>
-      <Header />
+    <Cart />
+    <Header />
       <main>
         <Meals />
       </main>
